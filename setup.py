6ce12728c69d932a16a45c3db35875912fc63a09
Grzegorz Dzięgielewski <jabbas@jabbas.eu>
2013-02-26 12:40:54 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Author: Grzegorz Dziegielewski
+
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "dotfiles",
+    version = "0.1",
+    author = "Grzegorz Dzięgielewski",
+    author_email = "jabbas@jabbas.eu",
+    description = "simple wrapper over git to manage dotfiles",
+    license = "GPLv3+",
+    keywords = "dotfiles git wrapper",
+    url = "http://git.jabbas.eu/dotfiles.git",
+    packages=find_packages(),
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Topic :: Utilities",
+        "Topic :: Software Development :: Version Control",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+    ],
+)
